705cd1212c5187e9172851c1fc3e8a170bd49d01
nareshkumarcs28 <nareshkumarcs28@gmail.com>
2025-09-22 01:09:01 +0530

    playing wiith routes

--- src/app.js
+++ src/app.js
@@ -3,27 +3,31 @@ const app = express();
 
 
 
-app.get("/user", (req,res)=>{
-    res.send("im GET testing");
-});
-app.post("/user", (req,res)=>{
-    res.send("im POST testing");
-});
+// app.get("/user", (req,res)=>{
+//     res.send({ fisrtName:"naresh", lastName:"Kumar"});
+// });
 
-
-app.patch("/user", (req,res)=>{
-    res.send("im PATCH testing");
-});
-
-
-app.delete("/user", (req,res)=>{
-    res.send("im DELETE testing");
-});
-
-
-
-
-
+app.get("/user", (req,res, next)=>{
+    console.log(" running 1")
+    // res.send("hello i am user1");
+    next();
+},
+(req,res, next)=>{
+    console.log(" running 2")
+    // res.send("hello i am user1");
+     next();
+},
+(req,res, next)=>{
+    console.log(" running 3")
+    // res.send("hello i am user1");
+     next();
+},
+(req,res, next)=>{
+    console.log(" running 4")
+    res.send("hello i am user4");
+    
+}
+);
 app.listen(3000,()=>{
     console.log("running on port 3000......");
 });;
